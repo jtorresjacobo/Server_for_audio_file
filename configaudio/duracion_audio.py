2536403b128d515fb366b32ae00842eb2334d44c
Diego F <diegoalonsofelix05@gmail.com>
2018-10-19 17:24:27 -0500

    correcion
    hora minuto segundo

--- configaudio/duracion_audio.py
+++ configaudio/duracion_audio.py
@@ -1,19 +1,30 @@
 
 import subprocess
 import re
+import os
 
 path='/home/diego/Escritorio/quechua_next.wav'
 
-process = subprocess.Popen(['ffmpeg',  '-i',path], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-stdout, stderr = process.communicate()
-matches = re.search(r"Duration:\s{1}(?P<hours>\d+?):(?P<minutes>\d+?):(?P<seconds>\d+\.\d+?),", stdout, re.DOTALL).groupdict()
+def Audio_duracion(paths):
+	
+	paths=path
 
-print (matches['hours']+":"+matches['minutes']+":"+matches['seconds'])
+	process = subprocess.Popen(['ffmpeg',  '-i',path], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	stdout, stderr = process.communicate()
+	matches = re.search(r"Duration:\s{1}(?P<hours>\d+?):(?P<minutes>\d+?):(?P<seconds>\d+\.\d+?),", stdout, re.DOTALL).groupdict()
+	
+	print(matches['hours'])
+	#matches['minutes']
+	#matches['seconds']
+	return j
+	
+
 #print matches['minutes']
+
 #print matches['seconds']
 
 
-
+Audio_duracion(path)
 
 
 
